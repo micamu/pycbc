3bd27325effef7dd0c905b63ea5ada93c6548113
Collin Capano <collin.capano@ligo.org>
2017-04-20 14:56:08 +0200

    fix bug in pool.py

--- pycbc/pool.py
+++ pycbc/pool.py
@@ -60,7 +60,7 @@ class BroadcastPool(multiprocessing.pool.Pool):
             The arguments for Pool.map
         """
         global _numdone
-        results = self.map(lockstep_fcn, [(len(self), fcn, args)] * len(self))
+        results = self.map(_lockstep_fcn, [(len(self), fcn, args)] * len(self))
         _numdone.value = 0
         return results
 
